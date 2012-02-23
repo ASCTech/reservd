1dd5214131e3e0c19574b4060ee414b8daf7028c
John Colvin <colvin.68@osu.edu>
2012-02-23 17:27:54 -0500

    Added validation of accepted types to attribute_type

--- app/models/attribute_type.rb
+++ app/models/attribute_type.rb
@@ -2,4 +2,8 @@ class AttributeType < ActiveRecord::Base
 
   has_many :attributes
 
+  DATATYPES = ['boolean', 'string', 'integer', 'date']
+  
+  validates_inclusion_of :data_type, :in => DATATYPES, :allow_nil => false
+
 end
