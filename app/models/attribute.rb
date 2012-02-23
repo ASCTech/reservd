ea319e9dbb347f095fb51f84edf5b1e89ca77fef
John Colvin <colvin.68@osu.edu>
2012-02-23 17:11:21 -0500

    Override update attribute to handle updating value of attribute

--- app/models/attribute.rb
+++ app/models/attribute.rb
@@ -3,8 +3,18 @@ class Attribute < ActiveRecord::Base
   belongs_to :resource
   belongs_to :attribute_type
 
+  delegate :data_type, :to => :attribute_type
+
   def value
-    send "#{attribute_type.data_type}_value"
+    send "#{data_type}_value"
+  end
+
+  def update_attribute(name, val)
+    if name.to_sym == :value
+      update_attribute("#{data_type}_value", val)
+    else
+      super
+    end
   end
 
 end
