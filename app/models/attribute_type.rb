d3f5097935ee3c35289c7a6ad070298f9ddd3603
John Colvin <colvin.68@osu.edu>
2012-02-24 04:32:06 -0500

    Add DS_STORE to gitignore

--- app/models/attribute_type.rb
+++ app/models/attribute_type.rb
@@ -3,7 +3,7 @@ class AttributeType < ActiveRecord::Base
   has_many :attributes
 
   DATATYPES = ['boolean', 'string', 'integer', 'date']
-  
+
   validates_inclusion_of :data_type, :in => DATATYPES, :allow_nil => false
 
 end
