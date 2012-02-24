f20f810f6cc09a6cab8788061fc628932e905c84
John Colvin <colvin.68@osu.edu>
2012-02-24 13:59:27 -0500

    Made the previous tests much cleaner

--- spec/models/attribute_spec.rb
+++ spec/models/attribute_spec.rb
@@ -17,40 +17,15 @@ describe Attribute do
 
   describe 'interacting with value' do
 
-    it 'should use boolean_value for boolean attributes' do
-      boolean_attribute.value.should be_nil
-      boolean_attribute.value.should == boolean_attribute.boolean_value
-      new_value = false
-      boolean_attribute.update_attribute(:value, new_value)
-      boolean_attribute.value.should == new_value
-      boolean_attribute.value.should == boolean_attribute.boolean_value
-    end
-
-    it 'should use date_value for date attributes' do
-      date_attribute.value.should be_nil
-      date_attribute.value.should == date_attribute.date_value
-      new_value = Date.today
-      date_attribute.update_attribute(:value, new_value)
-      date_attribute.value.should == new_value
-      date_attribute.value.should == date_attribute.date_value
-    end
-
-    it 'should use string_value for date attributes' do
-      string_attribute.value.should be_nil
-      string_attribute.value.should == string_attribute.string_value
-      new_value = 'This is a string value'
-      string_attribute.update_attribute(:value, new_value)
-      string_attribute.value.should == new_value
-      string_attribute.value.should == string_attribute.string_value
-    end
-
-    it 'should use integer_value for integer attributes' do
-      integer_attribute.value.should be_nil
-      integer_attribute.value.should == integer_attribute.integer_value
-      new_value = 43
-      integer_attribute.update_attribute(:value, new_value)
-      integer_attribute.value.should == new_value
-      integer_attribute.value.should == integer_attribute.integer_value
+    it 'should use the correct column' do
+      types = { boolean_attribute => false, date_attribute => Date.today, string_attribute => 'This is a string', integer_attribute => 43 }
+      types.each do |attribute, new_value|
+        attribute.value.should be_nil
+        attribute.value.should == attribute.send("#{attribute.data_type}_value")
+        attribute.update_attribute(:value, new_value)
+        attribute.value.should == new_value
+        attribute.value.should == attribute.send("#{attribute.data_type}_value")
+      end
     end
 
   end
