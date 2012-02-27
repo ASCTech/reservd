83b57c21b72f1bfcb0f5788b8669b640ca6a2d0a
John Colvin <colvin.68@osu.edu>
2012-02-27 11:01:31 -0500

    Added interaction with attributes on a resource by their name

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,4 +3,14 @@ class Resource < ActiveRecord::Base
   belongs_to :resource_type
   has_many :attributes
 
+  def method_missing(method, *args, &block)
+    super unless attributes.map(&:name).include? method.to_s
+ 
+    attributes.each do |attribute|
+      next unless attribute.name == method.to_s
+      return attribute.value
+    end
+    super
+  end
+
 end
--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -8,4 +8,15 @@ describe Resource do
     resource.should be_valid
   end
 
+  describe 'interaction with attributes' do
+
+    let(:boolean_attribute) { create(:boolean_attribute) }
+
+    it 'should allow accessing an attribute by its name' do
+      resource.attributes.create!(boolean_attribute.attributes)
+      resource.send(boolean_attribute.name).should == boolean_attribute.value
+    end
+
+  end
+
 end
