d5e47c6e9ec918a6a048886a334a25648804ed86
John Colvin <colvin.68@osu.edu>
2012-02-27 17:23:51 -0500

    Added edit and update for resource

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -8,4 +8,14 @@ class ResourcesController < ApplicationController
     @resource = Resource.find(params[:id])
   end
 
+  def edit
+    @resource = Resource.find(params[:id])
+  end
+
+  def update
+    resource = Resource.find(params[:id])
+    resource.update_attributes(params[:resource])
+    redirect_to resource
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Reservd::Application.routes.draw do
 
-  resources :resources, :only => [:index, :show]
+  resources :resources, :only => [:index, :show, :edit, :update]
 
 end
