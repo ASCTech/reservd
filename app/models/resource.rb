0717e6c9073ea485387a030325144e31fa646d03
James Muir <muir.29@osu.edu>
2012-03-01 17:18:12 -0500

    added uniqueness to name of a resource

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,6 +3,11 @@ class Resource < ActiveRecord::Base
   belongs_to :resource_type
   has_many :properties
 
+  validates_uniqueness_of :name, :scope => :resource_type_id
+  validates_presence_of :resource_type_id
+  validates :resource_type, :existence => true
+
+
   def method_missing(method, *args, &block)
     if method.to_s[-1,1] == '='
       properties.each do |property|
